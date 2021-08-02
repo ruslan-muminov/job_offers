8de3b21b8a99e52e104d141e21a9f0c26ebd72c3
Ruslan Muminov <ruslan.muminov1@gmail.com>
2021-08-01 21:10:12 -0400

    tiny refactoring

--- lib/job_offers.ex
+++ lib/job_offers.ex
@@ -9,49 +9,67 @@ defmodule JobOffers do
 
   @total_key "total"
   @error_key "error"
+  @stream_opts read_ahead: 100_000
 
-  # JobOffers.test_parse_professions()
-  def test_parse_professions() do
-    "/root/work/job_offers/technical-test-professions.csv"
-    |> File.stream!(read_ahead: 100_000)
+  # JobOffers.parse_professions()
+  def parse_professions do
+    professions_path()
+    |> File.stream!(@stream_opts)
     |> CsvParser.parse_stream
     |> Enum.reduce(%{}, fn [id, name, category], acc ->
       Map.put(acc, id, {name, category})
     end)
   end
 
-  # JobOffers.test_parse_jobs()
-  def test_parse_jobs() do
-    professions = test_parse_professions()
+  # JobOffers.category_continent_job_offers_count()
+  def category_continent_job_offers_count do
+    professions = parse_professions()
 
     result =
-      "/root/work/job_offers/technical-test-jobs.csv"
-      |> File.stream!(read_ahead: 100_000)
+      jobs_path()
+      |> File.stream!(@stream_opts)
       |> CsvParser.parse_stream
-      |> Enum.reduce({%{}, %{}, %{}, %{}}, fn [profession_id, _, _, office_lat, office_lon] = row,
-                                                  {crossings, continents, categories, service} ->
-        try do
-          {:ok, continent_id} = DefineContinent.get_continent_by_coordinates(office_lat, office_lon)
-          {:ok, continent_name} = DefineContinent.get_continent_name_by_id(continent_id)
-          {_, category} = Map.get(professions, profession_id)
-
-          {
-            increment_count(crossings, {continent_name, category}),
-            increment_count(continents, continent_name),
-            increment_count(categories, category),
-            increment_count(service, @total_key)
-          }
-        rescue
-          _e ->
-            Logger.warn("incorrect data: #{inspect(row)}")
-            {crossings, continents, categories, increment_count(service, @error_key)}
-        end
-      end)
+      |> category_continent_reduce(professions)
 
     Printer.convert_and_print(result)
     {:ok, result}
   end
 
+
+  defp category_continent_reduce(rows_stream, professions) do
+    rows_stream
+    |> Enum.reduce({%{}, %{}, %{}, %{}}, fn [profession_id, _, _, office_lat, office_lon] = row,
+                                                {crossings, continents, categories, service} ->
+      try do
+        {:ok, continent_id} = DefineContinent.get_continent_by_coordinates(office_lat, office_lon)
+        {:ok, continent_name} = DefineContinent.get_continent_name_by_id(continent_id)
+        {_, category} = Map.get(professions, profession_id)
+
+        {
+          increment_count(crossings, {continent_name, category}),
+          increment_count(continents, continent_name),
+          increment_count(categories, category),
+          increment_count(service, @total_key)
+        }
+      rescue
+        _e ->
+          Logger.warn("incorrect data: #{inspect(row)}")
+          {crossings, continents, categories, increment_count(service, @error_key)}
+      end
+    end)
+  end
+
+  defp jobs_path do
+    :job_offers
+    |> :code.priv_dir()
+    |> Path.join("technical-test-jobs.csv")
+  end
+
+  defp professions_path do
+    :job_offers
+    |> :code.priv_dir()
+    |> Path.join("technical-test-professions.csv")
+  end
 
   defp increment_count(map, key) do
     {_, new_map} =
